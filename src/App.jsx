c6b78cd68607cddf8ae29dd4a199ae4011fb2789
nhinhiii <nhinguyenyen0908@gmail.com>
2025-08-01 21:40:03 -0700

    Set up and add tailwindCSS for background

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import SunnyBackground from "./backgrounds/SunnyBackground";
+
+function App() {
+  return (
+    <div>
+      <SunnyBackground />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/backgrounds/SunnyBackground.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SunnyBackground = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden bg-sunny"></div>
+  );
+};
+
+export default SunnyBackground;
