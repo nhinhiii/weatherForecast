0b08016d539222eb0fc022a9956fa8f5f49e3bb6
nhinhiii <nhinguyenyen0908@gmail.com>
2025-08-04 22:45:14 -0700

    add humid, wind, sunrise, sunset icon and html

--- src/Route/WeatherForcast.jsx
+++ src/Route/WeatherForcast.jsx
@@ -2,37 +2,38 @@ import React, { useEffect, useState } from "react";
 import { getWeatherTheme } from "../lib/weatherUtils";
 import { weatherThemes } from "../config/weatherThemes";
 import { capitalizeWords } from "../lib/capitilize";
+import wind from "../assets/Icons/wind.svg";
+import sunrise from "../assets/Icons/sunrise.svg";
+import sunset from "../assets/Icons/sunset.svg";
+import humid from "../assets/Icons/humid.svg";
 
 const WeatherForecast = () => {
   const [cityInput, setCityInput] = useState("");
   const [weatherData, setWeatherData] = useState(null);
-  const [err, setErr] = useState(null);
+  const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const weatherAPI = import.meta.env.VITE_WEATHER_API;
 
   const search = async (city) => {
-    if (!city) {
-      return;
-    }
+    if (!city) return;
+
     setIsLoading(true);
-    setErr(null);
+    setError(null);
     try {
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${weatherAPI}`;
       const response = await fetch(url);
       if (!response.ok) {
-        throw new Error("City not found. Please try again!");
+        throw new Error(`City not found. Please try again.`);
       }
       const data = await response.json();
       setWeatherData(data);
     } catch (err) {
-      setErr(err.message);
-      setWeatherData(null);
+      setError(err.message);
     } finally {
       setIsLoading(false);
     }
   };
 
-  //default city
   useEffect(() => {
     search("New York");
   }, []);
@@ -42,11 +43,17 @@ const WeatherForecast = () => {
     search(cityInput);
   };
 
+  const themeName = weatherData ? getWeatherTheme(weatherData) : "Sunny";
+  const currentTheme = weatherThemes[themeName];
+
   return (
     <div className="relative h-screen w-full">
-      {weatherData
-        ? weatherThemes[getWeatherTheme(weatherData)].background
-        : weatherThemes["Sunny"].background}
+      {currentTheme.background}
+
+      <img
+        src={currentTheme.src}
+        className="absolute w-50 sm:30 h-auto top-20 left-35"
+      />
 
       <form
         onSubmit={handleSearch}
@@ -56,7 +63,7 @@ const WeatherForecast = () => {
           type="text"
           onChange={(e) => setCityInput(e.target.value)}
           value={cityInput}
-          placeholder="Type the city name..."
+          placeholder="Search for a city..."
           className="p-2 rounded-md text-white bg-black/30 border border-white/20 backdrop-blur-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
         />
         <button
@@ -69,47 +76,83 @@ const WeatherForecast = () => {
       </form>
 
       <div className="absolute inset-0 flex items-center justify-center z-10">
-        {isLoading && <div className="text-white text-2xl"> Loading ... </div>}
-        {err && (
+        {error && (
           <div className="text-red-400 bg-red-900 bg-opacity-50 text-center p-10 rounded-lg">
-            {err}
+            {error}
           </div>
         )}
-        {weatherData && !isLoading && !err && (
-          <div className="text-[#FFFAFA] text-center">
+
+        {weatherData && !error && (
+          <div
+            className={`text-[#FFFAFA] text-center transition-opacity duration-500 ${
+              isLoading ? "opacity-30" : "opacity-100"
+            }`}
+          >
             {(() => {
-              const themeName = getWeatherTheme(weatherData);
-              const currentTheme = weatherThemes[themeName];
-              const date = new Date(weatherData.dt * 1000);
+              const utcSeconds = weatherData.dt;
+              const cityOffsetSeconds = weatherData.timezone;
+              const correctedTimestamp =
+                (utcSeconds + cityOffsetSeconds) * 1000;
+              const cityDate = new Date(correctedTimestamp);
               const options = {
                 month: "short",
                 day: "2-digit",
                 year: "numeric",
+                timeZone: "UTC",
               };
-              const formattedDate = date.toLocaleDateString("en-US", options);
+              const formattedDate = cityDate.toLocaleDateString(
+                "en-US",
+                options
+              );
 
               return (
-                <>
-                  <p className="text-2xl font-mono"> {formattedDate} </p>
-                  <p className="text-lg font-serif mt-4"> {weatherData.name}</p>
+                <div>
+                  <p className="text-2xl font-mono">{formattedDate}</p>
+                  <p className="text-lg font-serif mt-4">{weatherData.name}</p>
                   <p className="text-7xl mt-4">
-                    {" "}
                     {Math.floor(weatherData.main.temp)}°
                   </p>
                   <p
                     className="text-2xl font-medium mt-4 capitalize"
                     style={{ color: `${currentTheme.themeColor}` }}
                   >
-                    {capitalizeWords(weatherData.weather[0].description)}
+                    {weatherData.weather[0].description}
                   </p>
-                  <div className="flex justify-center gap-12 mt-4">
-                    <p> Max: {Math.floor(weatherData.main.temp_max)}°</p>
-                    <p> Min: {Math.floor(weatherData.main.temp_min)}°</p>
+                  <div className="flex justify-center gap-6 mt-4">
+                    <p>Max: {Math.floor(weatherData.main.temp_max)}°</p>
+                    <p>Min: {Math.floor(weatherData.main.temp_min)}°</p>
                   </div>
-                </>
+
+                  <div className="grid grid-cols-2 justify-center mt-8 gap-30">
+                    <div className="flex flex-col mt-8 gap-10">
+                      <div className="flex">
+                        <img src={wind} size={16} />
+                        <p> {weatherData.wind.speed} mph</p>
+                      </div>
+                      <div className="flex">
+                        <img src={humid} size={16} />
+                        <p> {weatherData.wind.speed} mph</p>
+                      </div>
+                    </div>
+                    <div className="flex flex-col mt-8 gap-10">
+                      <div className="flex">
+                        <img src={sunrise} size={16} />
+                        <p> {weatherData.wind.speed} mph</p>
+                      </div>
+                      <div className="flex">
+                        <img src={sunset} size={16} />
+                        <p> {weatherData.wind.speed} mph</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               );
             })()}
           </div>
+        )}
+
+        {isLoading && (
+          <div className="absolute text-white text-2xl">Loading...</div>
         )}
       </div>
     </div>
